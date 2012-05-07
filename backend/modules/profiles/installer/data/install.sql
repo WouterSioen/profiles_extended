4243f3ac9a7b503ec1ad86ecbb649a0dfbb641e6
Wouter Sioen <wouter.sioen@gmail.com>
2012-05-07 14:42:47 +0100

    add profiles_activity table to install.sql

--- backend/modules/profiles/installer/data/install.sql
+++ backend/modules/profiles/installer/data/install.sql
@@ -1,3 +1,14 @@
+CREATE TABLE IF NOT EXISTS `profiles_activity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `action` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `status` enum('visible','hidden') NOT NULL,
+  `created_on` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `profiles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
